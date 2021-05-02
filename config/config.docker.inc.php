9d22e417bd6056267bf98959fb47e00556171534
Your Name <s@dondley.com>
2021-05-02 12:43:25 +0000

    add sdondley_devel branch

---
+++ config/config.docker.inc.php
@@ -0,0 +1,11 @@
+<?php
+  $config['db_dsnw'] = 'sqlite:////var/roundcube/db/sqlite.db?mode=0646';
+  $config['db_dsnr'] = '';
+  $config['default_host'] = 'ssl://email.dondley.com';
+  $config['default_port'] = '993';
+  $config['smtp_server'] = 'tls://email.dondley.com';
+  $config['smtp_port'] = '587';
+  $config['temp_dir'] = '/tmp/roundcube-temp';
+  $config['skin'] = 'larry';
+  $config['plugins'] = array_filter(array_unique(array_merge($config['plugins'], ['archive', 'zipdownload'])));
+  
---
+++ config/config.inc.php
@@ -0,0 +1,7 @@
+<?php
+    $config['plugins'] = [];
+    $config['log_driver'] = 'stdout';
+    $config['zipdownload_selection'] = true;
+    $config['des_key'] = 'k3z2ykPmGfoRW5+mNp0gz7La';
+    include(__DIR__ . '/config.docker.inc.php');
+    
